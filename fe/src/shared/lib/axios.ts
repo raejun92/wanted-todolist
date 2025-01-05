a133d5f4ae2e68eb4f4fc1407e1d5a64af279b87
raejun92 <cvcv1324@naver.com>
2025-01-06 00:01:14 +0900

    axios interceptor

--- fe/src/shared/lib/axios.ts
+++ fe/src/shared/lib/axios.ts
@@ -7,4 +7,18 @@ const apiClient = axios.create({
   },
 });
 
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+
+    if (token) {
+      config.headers.Authorization = token;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export default apiClient;

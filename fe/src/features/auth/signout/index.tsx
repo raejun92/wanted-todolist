e97cbf2fb403ceb1b79ecd31589ad08881a6011f
raejun92 <cvcv1324@naver.com>
2025-01-06 00:42:38 +0900

    sign out

---
+++ fe/src/features/auth/signout/index.tsx
@@ -0,0 +1,16 @@
+import { authProvider } from '@/auth';
+import { Button } from '@/shared/components/ui/button';
+import { useNavigate } from 'react-router';
+
+export default function Signout() {
+	const navigate = useNavigate();
+
+  const handleSignout = () => {
+    authProvider.signout(); // 토큰 삭제 및 인증 상태 초기화
+    navigate('/'); // 로그인 페이지로 리다이렉트
+  };
+
+	return <Button onClick={handleSignout} className="bg-blue-500 hover:bg-blue-700">
+			Sign out
+		</Button>
+}
